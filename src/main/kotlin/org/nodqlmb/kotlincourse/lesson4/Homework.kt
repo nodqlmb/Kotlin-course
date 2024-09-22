b13ce61a5987d55701ad04c5c8cb18bbe9073830
ivjsu <nodqlmb@gmail.com>
2024-09-22 12:17:25 +0300

    Initial commit

---
+++ src/main/kotlin/org/nodqlmb/kotlincourse/lesson4/Homework.kt
@@ -0,0 +1,40 @@
+package org.nodqlmb.kotlincourse.lesson4
+
+val myInt: Int = 2
+val myLong: Long = 98765432123456789L
+val myFloat: Float = 23.45f
+val myDouble: Double = 0.123456789
+val myString: String = "Kotlin & Java"
+val myBoolean: Boolean = false
+val myChar: Char = 'c'
+val mySecondInt: Int = 500
+val mySecondLong: Long = 4294967296L
+val mySecondFloat: Float = 18.0f
+val mySecondDouble: Double = -0.001
+val mySecondString: String = "OpenAI"
+val myThirdString: String = "true" // кавычки
+val mySecondChar: Char = '9'
+val myThirdInt: Int = 2048
+val myThirdLong: Long = 10000000000L
+val myThirdFloat: Float = 5.75f
+val myThirdDouble: Double = 1.414
+val myFourthString: String = "Artificial Intelligence"
+val mySecondBoolean: Boolean = false
+val myThirdChar: Char = '@'
+val myFourthInt: Int = 1024
+val myFourthLong: Long  = 1234567890123L
+val myFourthFloat: Float = 10.01f
+val myFourthDouble: Double = -273.15
+val myFifthString: String = "SpaceX"
+val myThirdBoolean: Boolean = true
+//“🤯” // кавычки кривые
+//‘65535’ // еще одни кривые кавычки
+val myFifthLong: Long = 72057594037927935L
+val myFifthFloat: Float = 2.71828f
+val myFifthDouble: Double = 101.0101
+val mySixthString: String = "Quantum Computing"
+val myFourthBoolean: Boolean = false
+val myFourthChar: Char = 'x'
+val myFifthInt: Int = 314
+val mySixthFloat: Float = 6.626f
+val mySixthDouble: Double = 0.007
