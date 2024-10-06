3b00a44cdf80c239675308b56c0a28f04e20fb15
ivjsu <nodqlmb@gmail.com>
2024-10-07 01:52:27 +0300

    Тортик

---
+++ src/main/kotlin/org/nodqlmb/kotlincourse/lesson9/Bday.kt
@@ -0,0 +1,13 @@
+package org.nodqlmb.kotlincourse.lesson9
+
+fun main() {
+    println("Саша, с днем рождения!")
+    println("")
+    println("┈┈┈☆☆☆☆☆☆☆┈┈┈")
+    println("┈┈╭┻┻┻┻┻┻┻┻┻╮┈┈")
+    println("┈┈┃╱╲╱╲╱╲╱╲╱┃┈┈")
+    println("┈╭┻━━━━━━━━━┻╮┈")
+    println("┈┃╱╲╱╲╱╲╱╲╱╲╱┃┈")
+    println("┈┗━━━━━━━━━━━┛┈ ")
+
+}
